74f25be893a55adcf5aaa500451d20ffd8fb8ab0
jnlh863 <lopezhernandezjahirnicolas@gmail.com>
2024-07-24 02:46:56 -0600

    tets

--- Models/Dtos/Account-Profile-Dtos/AccountInfoDto.cs
+++ Models/Dtos/Account-Profile-Dtos/AccountInfoDto.cs
@@ -5,7 +5,7 @@ namespace AppVidaSana.Models.Dtos.Cuenta_Perfil_Dtos
     public class AccountInfoDto
     {
         [JsonRequired] public Guid accountID { get; set; }
-
+         
         [JsonRequired] public string username { get; set; } = null!;
 
         [JsonRequired] public string email { get; set; } = null!;
