245c7ed2905b8e33dbc3b1f28587d5f59c1c08f9
jnlh863 <lopezhernandezjahirnicolas@gmail.com>
2024-12-26 18:56:16 -0600

    feat: add logout

--- Controllers/Authentication-Authorization-Controller.cs
+++ Controllers/Authentication-Authorization-Controller.cs
@@ -1,5 +1,6 @@
 ﻿using AppVidaSana.Api;
 using AppVidaSana.Exceptions;
+using AppVidaSana.Exceptions.Account_Profile;
 using AppVidaSana.Exceptions.Cuenta_Perfil;
 using AppVidaSana.Models.Dtos.Account_Profile_Dtos;
 using AppVidaSana.Models.Dtos.Reset_Password_Dtos;
@@ -97,10 +98,12 @@ namespace AppVidaSana.Controllers
         /// </summary>
         /// <response code="200">The tokens were successfully generated.</response>
         /// <response code="400">Returns a message that the requested action could not be performed.</response>
+        /// <response code="401">Returns a message indicating the refresh token has expired.</response>  
         /// <response code="409">Returns a series of messages indicating that some values are invalid.</response>
         /// <response code="500">Returns a message indicating internal server errors.</response>
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoginResponse))]
         [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ExceptionMessage))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ExceptionMessage))]
         [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ExceptionListMessages))]
         [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ExceptionMessage))]
         [ApiKeyAuthorizationFilter]
@@ -128,6 +131,15 @@ namespace AppVidaSana.Controllers
 
                 return StatusCode(StatusCodes.Status400BadRequest, new { message = response.message, status = response.status });
             }
+            catch (RefreshTokenExpirationException ex)
+            {
+                ExceptionMessage response = new ExceptionMessage
+                {
+                    status = ex.Message
+                };
+
+                return StatusCode(StatusCodes.Status401Unauthorized, new { message = response.message, status = response.status });
+            }
             catch (ErrorDatabaseException ex)
             {
                 ExceptionListMessages response = new ExceptionListMessages
@@ -148,5 +160,38 @@ namespace AppVidaSana.Controllers
             }
         }
 
+        /// <summary>
+        /// This controller performs the logout action.
+        /// </summary>
+        /// <response code="200">The closing session was a success.</response>
+        /// <response code="400">Returns a message that the requested action could not be performed.</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LogoutResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ExceptionMessage))]
+        [ApiKeyAuthorizationFilter]
+        [HttpDelete("logout/{accountID:guid}")]
+        [Produces("application/json")]
+        public async Task<IActionResult> LogoutAccountAsync(Guid accountID)
+        {
+            try
+            {
+                var status = await _AuthService.LogoutAccountAsync(accountID, HttpContext.RequestAborted);
+
+                LogoutResponse response = new LogoutResponse
+                {
+                    status = status
+                };
+
+                return StatusCode(StatusCodes.Status200OK, new { message = response.message, status = response.status });
+            }
+            catch (UnstoredValuesException ex)
+            {
+                ExceptionMessage response = new ExceptionMessage
+                {
+                    status = ex.Message
+                };
+
+                return StatusCode(StatusCodes.Status400BadRequest, new { message = response.message, status = response.status });
+            }
+        }
     }
 }
---
+++ ProducesResponseType/Authenticator/LogoutResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AppVidaSana.ProducesResponseType.Authenticator
+{
+    public class LogoutResponse
+    {
+        public string message { get; set; } = "Ok.";
+
+        public string status { get; set; }
+    }
+}
