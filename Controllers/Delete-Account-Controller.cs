45940651f293f20a623f1d64247618c934e71ffd
JahirNicolas <jahir@alebrije-estudios.com>
2025-08-14 21:16:32 -0600

    feat: pagina para eliminar cuentas

---
+++ Controllers/Delete-Account-Controller.cs
@@ -0,0 +1,43 @@
+﻿using AppVidaSana.ProducesResponseType;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http.Timeouts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppVidaSana.Controllers
+{
+    [EnableCors("RulesCORS")]
+    [ApiController]
+    [Tags("App - Account_Profile")]
+    [ApiExplorerSettings(GroupName = "app")]
+    [Route("delete-account")]
+    [RequestTimeout("CustomPolicy")]
+    public class DeleteAccountController : Controller
+    {
+        /// <summary>
+        /// This is the driver for the delete account view.
+        /// </summary>
+        /// <response code="404">Returns a message indicating that the page could not be loaded correctly.</response>
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ExceptionMessage))]
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult DeleteAccountPatient()
+        {
+            try
+            {
+                return View("~/Views/DeleteAccountPage/DeleteAccountPatient.cshtml");
+            }
+            catch (Exception)
+            {
+                ExceptionMessage response = new ExceptionMessage
+                {
+                    message = "Hubo un error, inténtelo de nuevo.",
+                    status = "No se cargo completamente la página"
+                };
+
+                return StatusCode(StatusCodes.Status404NotFound, new { message = response.message, status = response.status });
+
+            }
+        }
+    }
+}
--- Controllers/Reset-Password-Controller.cs
+++ Controllers/Reset-Password-Controller.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http.Timeouts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
-using Azure;
 
 namespace AppVidaSana.Controllers
 {
